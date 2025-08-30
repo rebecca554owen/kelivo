93cd9fc9e34d832933d170531aafd443f8ac0f6f
psyche <dahldoor@gmail.com>
2025-08-30 16:01:06 +0800

    feat: add macOS support and update dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:kelivo/main.dart';
+import 'package:Kelivo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
