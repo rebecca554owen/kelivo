af67308316e2ac48c94472a9e9e6dab8e7c40b6d
psyche <dahldoor@gmail.com>
2025-10-24 14:46:03 +0800

    feat: add katkwaipilot and inclusionai brand assets

--- lib/utils/brand_assets.dart
+++ lib/utils/brand_assets.dart
@@ -68,5 +68,7 @@ class BrandAssets {
     MapEntry(RegExp(r'brave'), 'brave-color.svg'),
     MapEntry(RegExp(r'jina'), 'jina-color.svg'),
     MapEntry(RegExp(r'searxng'), 'searxng-color.svg'),
+    MapEntry(RegExp(r'kat'), 'katkwaipilot-color.svg'),
+    MapEntry(RegExp(r'inclusionai'), 'ling.png'),
   ];
 }
