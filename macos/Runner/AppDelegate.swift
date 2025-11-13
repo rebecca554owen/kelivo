6742ff0568ead62f059eb914e2101db63703a338
psyche <dahldoor@gmail.com>
2025-11-13 22:31:59 +0800

    feat: keep app running after closing window

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,16 @@ import FlutterMacOS
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return false
+  }
+
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    if !flag {
+        for window in sender.windows {
+            window.makeKeyAndOrderFront(self)
+        }
+    }
+
     return true
   }
 
